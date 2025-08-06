2a635e82b4503ab608f71775112426f1edba9b83
Nathan Fouere <nathanfouere@tutanota.com>
2025-08-06 21:35:51 +0200

    start adding pipelin

--- flake.nix
+++ flake.nix
@@ -1,10 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  #source : https://nixcademy.com/posts/cpp-with-nix-in-2023-part-1-shell/
+  description = "Nix flake for C++ snake";
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [
-    gcc
-    SDL2
-    pkg-config
-    cpplint
-  ];
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
+      ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gcc
+            SDL2
+            pkg-config
+            cpplint
+          ];
+        };
+      };
+    };
 }
